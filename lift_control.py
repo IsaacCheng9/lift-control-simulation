5ee6919d17b76767b080121e0de5bbbdb11dfcb0
Your Name <you@example.com>
2020-05-17 03:26:52 +0100

    Added more detailed module docstring

--- lift_control.py
+++ lift_control.py
@@ -1,4 +1,19 @@
-"""A system created to manage lift control, optimised for efficiency of use."""
+"""
+A system created to manage lift control, optimised for efficiency in distance
+travelled by the lift.
+
+The user is able to configure any number of floors, number of people, lift
+capacity, and the delay of intervals in the user interface.
+
+Their simulation can be visually displayed for up to five floors. For more
+than five floors, a non-visual display is provided. In both cases, the user
+can run their simulation with either the naive, mechanical lift algorithm, or
+my improved lift algorithm.
+
+The configured simulation remains the same until the user generates a new
+simulation, or changes the configuration settings. This enables the user to
+directly compare how the improved algorithm compares to the naive algorithm. 
+"""
 import json
 import logging
 import random
